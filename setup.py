135b33139ef6f0fbb10125249875b740c03d2804
Pebaz <pebazium@gmail.com>
2021-02-07 16:51:20 -0500

    moved cli to another lib

--- setup.py
+++ setup.py
@@ -5,10 +5,5 @@ setup(
 	version='0.1.0',
 	author='https://github.com/Pebaz',
 	py_modules=['dock'],
-	entry_points={
-		'console_scripts' : [
-			'dock=dock:dock'
-		]
-	},
 	install_requires=['dock-python-cli']
 )
