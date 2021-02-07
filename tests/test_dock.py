ce2e0c8e848dfd8187def4971ff85f01b6e47387
Pebaz <pebazium@gmail.com>
2021-02-07 14:58:09 -0500

    unit tests

--- tests/test_dock.py
+++ tests/test_dock.py
@@ -16,5 +16,56 @@ def case(input_, output=None):
     case(dict(returns=None, a=3), dict(returns=None, raises=None, a=3)),
 ])
 def test_dock(cases):
-    foo = dock(**cases['in'])(lambda: ...)
-    assert foo.__dock__ == cases['out'] or cases['in']
+    dock_obj = dock(**cases['in'])(lambda: ...)
+    assert dock_obj.__dock__ == cases['out'] or cases['in']
+
+
+@pytest.mark.parametrize('cases', [
+    case({}, {
+        'arguments': {},
+        'raises': None,
+        'returns': None,
+        'sections': {},
+        'short': None
+    }),
+    case({'c': 3}, {
+        'arguments': {'c': 3},
+        'raises': None,
+        'returns': None,
+        'sections': {},
+        'short': None
+    }),
+    case({'raises': 'What?'}, {
+        'arguments': {},
+        'raises': 'What?',
+        'returns': None,
+        'sections': {},
+        'short': None
+    }),
+    case({'c': 3, 'returns': 'What?'}, {
+        'arguments': {'c': 3},
+        'raises': None,
+        'returns': 'What?',
+        'sections': {},
+        'short': None
+    }),
+])
+def test_dock_arguments(cases):
+    def some_function(a: int, b: float) -> bool:
+        return a > b
+
+    dock_obj = dock(**cases['in'])(some_function)
+    assert dock_obj.__dock__ == cases['out'] or cases['in']
+
+
+@pytest.mark.parametrize('cases', [
+    case({}, dict(fields={})),
+    case({'a': 1}, dict(fields={'a': 1})),
+    case({'a': 1, 'Usage': '...'}, dict(fields={'a': 1, 'Usage': '...'})),
+])
+def test_dock_classes(cases):
+    class SomeClass:
+        "???"
+
+    dock_obj = dock(**cases['in'])(SomeClass)
+    assert dock_obj.__dock__ == cases['out'] or cases['in']
